6896e41015c5a75ae2174691b35720f2ff9650b8
okcul <117544641+okcul@users.noreply.github.com>
2023-05-27 18:08:37 -0400

    Update save_manip.py

--- py/save_manip.py
+++ py/save_manip.py
@@ -1,34 +1,37 @@
 from json import dump, load
 from os import listdir, remove
 
-def save(player : object, gameplay : dict):
+def file_save(player : object, gameplay : dict):
     player_dict = vars(player)
     player_name = player.name.lower()
 
-    with open(f"json/{player_name}.json", "w") as save_data:
+    with open(f"json/saves/{player_name}.json", "w") as save_data:
+        save_data.write('[ \n')
         dump(player_dict, save_data)
+        save_data.write(', \n')
         dump(gameplay, save_data)
+        save_data.write('\n ]')
     
-    return f"Data of '{player_name}' successfully saved to json/{player_name}.json!"
+    return f"Data of '{player_name}' successfully saved to json/saves/{player_name}.json!"
 
 
-def load(player_name : str):
+def file_load(player_name : str):
     player_name = player_name.lower()
 
     for filename in listdir("./json"):
         filename = str(filename)
         if player_name in filename.lower():
 
-            with open(f"json/{player_name}.json", "r") as save_data:
+            with open(f"json/saves/{player_name}.json", "r") as save_data:
                 player_dict = load(save_data)
 
-    return player_dict, f"Data of '{player_name}' successfully loaded from json/{player_name}.json!"
+    return player_dict, f"Data of '{player_name}' successfully loaded from json/saves/{player_name}.json!"
 
 
-def delete(player_name : str):
+def file_delete(player_name : str):
     player_name = player_name.lower()
 
-    for filename in listdir("./json"):
+    for filename in listdir("./json/saves"):
         filename = str(filename)
         if player_name in filename.lower():
 
@@ -40,7 +43,7 @@ def delete(player_name : str):
                     while True:
                         double_check = input(f"Type in '{player_name}' to fully confirm. Type 'cancel' to cancel.")
                         if double_check == player_name:
-                            remove(f"json/{player_name}.json")
+                            remove(f"json/saves/{player_name}.json")
                             return f"Data of '{player_name}' has successfully been deleted."                
                         elif double_check == "cancel":
                             return 0
