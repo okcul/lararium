44eb8da1b58a432d75901834e056b8f5a38bb473
okcul <117544641+okcul@users.noreply.github.com>
2023-05-27 18:15:38 -0400

    refactoring for gold attr

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ gameplay_stats = {
 }
 
 def menu():
-    You = Player("", [100, 100], 5, 0, ['potion'], 0, [1, 25, 0]) # name, hp (current, max), atk, status id, inventory, buff, level (level, exp needed, current exp)
+    You = Player("", [100, 100], 5, 0, ['potion'], 200, 0, [1, 25, 0]) # name, hp (current, max), atk, status id, inventory, gold, buff, level (level, exp needed, current exp)
     print("Hello player, and welcome to this little RPG I've made!")
     You.name = input("What is your name, player? ")
     print(f"Alright, hello then, {You.name}!")
